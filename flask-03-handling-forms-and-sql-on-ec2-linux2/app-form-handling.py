6f3a90e31c21af0a9a9946094594684f3b801f39
test1 <zeynep.arslan17@gmail.com>
2020-07-14 17:58:25 -0400

    added sqlite app

--- flask-03-handling-forms-and-sql-on-ec2-linux2/app-form-handling.py
+++ flask-03-handling-forms-and-sql-on-ec2-linux2/app-form-handling.py
@@ -15,7 +15,7 @@ def home():
 @app.route('/<greet>', methods=['GET'])
 def greet(name):
     if 'user' in request.args:
-        usr=request.args[user]
+        usr=request.args['user']
         return render_template('greet.html', user=usr)
     else:
         return render_template('greet.html', user='Send your username with "user" param in query string')
@@ -36,3 +36,33 @@ def login():
 if __name__ == '__main__':
     #app.run(debug=True)
     app.run('0.0.0.0', port=80)
+    ````
+#Write an application with database implementation using MySQL and save the complete code as app-with-mysql.py under hands-on/flask-03-handling-forms-and-sql-on-ec2-linux2 folder.
+```python
+# Import Flask modules
+
+# Create an object named app
+
+# Configure sqlite database
+
+# Create users table within MySQL db and populate with sample data
+# Execute the code below only once.
+# Write sql code for initializing users table..
+
+# Write a function named `find_emails` which find emails using keyword from the user table in the db,
+# and returns result as tuples `(name, email)`.
+
+# Write a function named `insert_email` which adds new email to users table the db.
+    # if user input are None (null) give warning
+    # if there is no same user name in the db, then insert the new one
+    # if there is user with same name, then give warning
+
+# Write a function named `emails` which finds email addresses by keyword using `GET` and `POST` methods,
+# using template files named `emails.html` given under `templates` folder
+# and assign to the static route of ('/')
+
+# Write a function named `add_email` which inserts new email to the database using `GET` and `POST` methods,
+# using template files named `add-email.html` given under `templates` folder
+# and assign to the static route of ('add')
+
+# Add a statement to run the Flask application which can be reached from any host on port 80.
